a0dcb22a90a700cd179b885328600df250a1c046
Eliabel Barreto <eliabeljunior@gmail.com>
2024-11-03 17:51:07 -0300

    Configurado o Entity Framework

---
+++ WorldCities/WorldCitiesApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCitiesApi.Data.Models;
+
+namespace WorldCitiesApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(): base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Country> Countries => Set<Country>();
+    }
+}
---
+++ WorldCities/WorldCitiesApi/Data/Models/CountryEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldCitiesApi.Data.Models
+{
+    public class CountryEntityTypeConfiguration
+    : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.ToTable("Countries");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).IsRequired();
+        }
+    }
+}
