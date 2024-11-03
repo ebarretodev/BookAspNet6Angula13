f00fcf93bddc3a4e7b415eea1b29859e8dd41a79
Eliabel Barreto <eliabeljunior@gmail.com>
2024-11-03 18:46:54 -0300

    Adicionado codigo para resolver o Erro de Cors em ambiente de desenvolvimento

--- WorldCities/WorldCitiesApi/Program.cs
+++ WorldCities/WorldCitiesApi/Program.cs
@@ -1,19 +1,35 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using WorldCitiesApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder
+            .AllowAnyOrigin() // Permite qualquer origem
+            .AllowAnyMethod() // Permite qualquer m�todo (GET, POST, etc.)
+            .AllowAnyHeader(); // Permite qualquer cabe�alho
+    });
+});
+
 // Add ApplicationDbContext and SQL Server support
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
+        builder.Configuration.GetConnectionString("ConnectionWorldCitiesDB")
         )
 );
 
@@ -24,6 +40,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowAllOrigins");
 }
 
 app.UseHttpsRedirection();
