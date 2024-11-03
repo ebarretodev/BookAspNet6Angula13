a0fb7693ff904f3fa48328055e5efe8a1484eb0a
Eliabel Barreto <eliabeljunior@gmail.com>
2024-11-03 18:47:56 -0300

    Criado código para receber a lista de cidades do server

--- WorldCities/WorldCitiesFront/src/app/services/api.service.ts
+++ WorldCities/WorldCitiesFront/src/app/services/api.service.ts
@@ -1,13 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { City } from '../models/city';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl: string
 
   constructor(private http: HttpClient) { 
-    const apiUrl = environment.apiUrl;
+    this.apiUrl = environment.apiUrl;
+  }
+
+  getCities(){
+    return this.http.get<City[]>(`${this.apiUrl}/Cities`)
   }
 }
