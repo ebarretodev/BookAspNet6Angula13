335ea9478cee818c280c46424489835204aec973
Eliabel Barreto <eliabeljunior@gmail.com>
2024-11-02 21:24:33 -0300

    Corrigido erro de cors

--- Cap02/HealthCheck/HealthCheckAPI/Program.cs
+++ Cap02/HealthCheck/HealthCheckAPI/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder
+            .AllowAnyOrigin() // Permite qualquer origem
+            .AllowAnyMethod() // Permite qualquer m�todo (GET, POST, etc.)
+            .AllowAnyHeader(); // Permite qualquer cabe�alho
+    });
+});
 
 var app = builder.Build();
 
@@ -14,6 +24,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowAllOrigins");
 }
 
 app.UseHttpsRedirection();
